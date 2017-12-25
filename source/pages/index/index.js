140a2fe79d105f13fa1a5380e501809b2c61d1fa
unknown <leancav97@gmail.com>
2017-12-26 01:06:33 +0200

    add main page without responsive

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,6 +1,4 @@
 import './index.scss';
 import 'normalize.css';
 
-import createMenu from '../../components/menu/menu';
-var menu = createMenu(['Products Features','More', 'Log In', 'Sign Up'], 'menu');
-document.body.appendChild(menu);
+
