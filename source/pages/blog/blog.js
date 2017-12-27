fe617ef2ef339b3bde0419d6029cc7b59782a170
unknown <leancav97@gmail.com>
2017-12-27 13:41:27 +0200

    add blog page

--- source/pages/blog/blog.js
+++ source/pages/blog/blog.js
@@ -1,6 +1,4 @@
 import './blog.scss';
 import 'normalize.css';
 
-import createMenu from '../../components/menu/menu';
-var menu = createMenu(['Products Features','More', 'Log In', 'Sign Up'], 'menu');
-document.body.appendChild(menu);
+
