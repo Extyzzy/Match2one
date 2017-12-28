2f8c21c9455c3025d742b0cd8fb172974ce167a0
unknown <leancav97@gmail.com>
2017-12-28 17:56:57 +0200

    added slider slick

--- source/pages/blog/blog.js
+++ source/pages/blog/blog.js
@@ -1,4 +1,42 @@
 import './blog.scss';
 import 'normalize.css';
 
+$(document).ready(function(){
+    $('.sliderContent').slick({
+        autoplay:true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
 
